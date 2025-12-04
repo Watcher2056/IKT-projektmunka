7d2b20c30ad85565f2913db98b878fd8621421a4
Rikardo66 <nemethkovacsrichard@gmail.com>
2025-12-04 18:59:30 +0100

    Push

--- SoundAngel/Sources/index_script.js
+++ SoundAngel/Sources/index_script.js
@@ -8,7 +8,7 @@ function showSlides(n) {
     if (n > totalSlides) slideIndex = 1;
     if (n < 1) slideIndex = totalSlides;
 
-    slidesContainer.style.transform = `translateX(-${(slideIndex - 1) * 1000}px)`;
+    slidesContainer.style.transform = `translateX(-${(slideIndex - 1) * 100}%)`;
 
     for (let i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
